fe1792a2956c17bc6dc1a5a825d7e4c783263d54
AishJoshi-2002 <aishjoshi2002@gmail.com>
2026-05-12 17:27:08 +0530

    routes added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Admission } from './features/admission/admission';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component: Home
+    },
+    {
+        path : 'admission',
+        component: Admission
+    },
+    {
+        path : 'admission',
+        component: Admission
+    },
+    {
+        path : 'admission',
+        component: Admission
+    }
+];
