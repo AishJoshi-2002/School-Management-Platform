dc5392eae255a8b4f20cda30abb7afc1e53e8eff
AishJoshi-2002 <aishjoshi2002@gmail.com>
2026-05-13 17:27:32 +0530

    Routes added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Admission } from './features/admission/admission';
+import { Events } from './features/events/events';
+import { Gallery } from './features/gallery/gallery';
+import { Notices } from './features/notices/notices';
+import {Teachers} from './features/teachers/teachers';
+import {Contact} from './pages/contact/contact';
+import {About} from './pages/about/about';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     {
@@ -12,11 +19,39 @@ export const routes: Routes = [
         component: Admission
     },
     {
-        path : 'admission',
-        component: Admission
+        path : 'events',
+        component: Events
     },
     {
-        path : 'admission',
-        component: Admission
+        path : 'gallery',
+        component: Gallery
+    },
+    {
+        path : 'notices',
+        component: Notices
+    },
+    {
+        path : 'teachers',
+        component: Teachers
+    },
+    {
+        path : 'contact-us',
+        component: Contact
+    },
+    {
+        path : 'about-us',
+        component: About
+    },
+    {
+        path : 'privacy-policy',
+        component: PrivacyPolicy
+    },
+    {
+        path : 'about-is',
+        component: About
+    },
+    {
+        path : 'about-is',
+        component: About
     }
 ];
